084fad92c1c607eb1569388e9f0301132626c1cc
Karina Hottinger <kamortegui70@gmail.com>
2026-07-15 11:17:55 -0700

    creating memory arrays, endpoint, and redirect

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ const adminCreds = {
   password: "123456s*",
 };
 
+const movements = [];
+const MovementModel = (name, difficulty, description, extraNotes) => ({
+  _id: movements.length + 1,
+  name,
+  difficulty,
+  description,
+  extraNotes: extraNotes || "",
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  deletedAt: null
+})
+
+const exercises = [];
+const ExerciseModel = (name, movementId, difficulty, description, extraNotes) => ({})
+
 // ----------- MOCK DB---------------------------- END
 
 // 1. create instance of express
@@ -31,10 +46,28 @@ app.post("/login", function (req, res) {
     });
   }
 
-  res.send({
-    message: "login successful",
-  });
+  res.redirect("/")
 });
+
+app.post("/movements", function (req, res) {
+  const { name, difficulty, description, extraNotes} = req.body;
+
+  const newMovement = MovementModel(name, difficulty, description, extraNotes);
+  movements.push(newMovement);
+
+  res.status(201).json({
+    message: "movement created successfully",
+    data: newMovement
+  })
+})
+
+app.get("/movements", function(req, res) {
+  res.status(200).json({
+    message: "movements fetched successfully",
+    data: movements,
+    total: movements.length
+  })
+})
 
 // 3. module.exports = app;
 // app.listen(8787, function () {
