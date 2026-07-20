a5b8ac644816138080be92d7232c3e475e1a0dfd
Karina Hottinger <kamortegui70@gmail.com>
2026-07-19 21:14:30 -0700

    Added backend form

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
-const express = require("express");
+
+// Load the express library
+const express = require("express"); 
+
+const cors = require("cors");
 
 // ----------- MOCK DB --------------------------- START
 const adminCreds = {
@@ -6,6 +10,9 @@ const adminCreds = {
   password: "123456s*",
 };
 
+
+// This array temporarily acts like your database. 
+// Keep in mind because this is an in memory array. Everything disappears when the server restarts. 
 const movements = [];
 const MovementModel = (name, difficulty, description, extraNotes) => ({
   _id: movements.length + 1,
@@ -19,23 +26,57 @@ const MovementModel = (name, difficulty, description, extraNotes) => ({
 })
 
 const exercises = [];
-const ExerciseModel = (name, movementId, difficulty, description, extraNotes) => ({})
+const ExerciseModel = (name, movementId, difficulty, description, extraNotes) => ({
+   name,
+  movementId:
+  difficulty,
+  description,
+  extraNotes: extraNotes || "",
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  deletedAt: null
+})
+
 
 // ----------- MOCK DB---------------------------- END
 
-// 1. create instance of express
+// 1. create instance of express 
+// creates express application (the app is your server)
+// everytime you see app.something you're adding another capability to your server
 const app = express();
 
 // middlewares
+// If someone asks for HTML, CSS, JS, or images inside the public folder, send them automatically
+// why browser can load index.html and Tailwind CSS w/o writing a route for each
 app.use(express.static("public"));
+// Express automatically converts JSON into a JS object
+// Allows you to write req.body.name instead of raw text
 app.use(express.json());
+app.use(cors());
+
+
 
 // 2. add ping endpoint
+//GET -> give me some information
 app.get("/ping", function (req, res) {
   console.log("req =", req.ip);
-  res.send("pong");
+  res.send({data: [{
+  "name": "test",
+  "difficulty": 1,
+  "description": "testing",
+  "extraNotes": "test"
+},
+
+{
+  "name": "test2",
+  "difficulty": 1,
+  "description": "testing",
+  "extraNotes": "test"
+}
+]});
 });
 
+// POST -> create something (save)
 app.post("/login", function (req, res) {
   const username = req.body.username;
   const password = req.body.password;
@@ -46,9 +87,10 @@ app.post("/login", function (req, res) {
     });
   }
 
-  res.redirect("/")
+  res.redirect("/home.html")
 });
 
+// creates a climbing movement
 app.post("/movements", function (req, res) {
   const { name, difficulty, description, extraNotes} = req.body;
 
@@ -61,11 +103,32 @@ app.post("/movements", function (req, res) {
   })
 })
 
+// returns all climbing movements
 app.get("/movements", function(req, res) {
   res.status(200).json({
     message: "movements fetched successfully",
     data: movements,
     total: movements.length
+  })
+})
+
+app.post("/exercises", function (req, res) {
+  const { name, movementId, difficulty, description, extraNotes} = req.body;
+
+  const newExercise = ExerciseModel(name, movementId, difficulty, description, extraNotes);
+  exercises.push(newExercise);
+
+  res.status(201).json({
+    message: "exercise created successfully",
+    data: newExercise
+  })
+})
+
+app.get("/exercises", function(req, res) {
+  res.status(200).json({
+    message: "exercises fetched successfully",
+    data: exercises,
+    total: exercises.length
   })
 })
 
